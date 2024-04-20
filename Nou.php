dade4bb56ee1147a940278f3560d051c50b791e4
Claudia <123874337+Claudiapf7@users.noreply.github.com>
2024-04-20 14:10:11 +0200

    Nou arxiu eliminar.php
    Elimina un producte de la botiga al apretar el botó eliminar

--- Nou.php
+++ Nou.php
@@ -2,19 +2,68 @@
 
 require_once('Connexio.php');
 
-Class Crear{
+Class Nou{
+    public function mostrarFormulari(){
+        // Imprime la estructura HTML del formulario de creación
+            echo '<!DOCTYPE html>
+                  <html lang="es">
+                  <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <title>Modificar producte</title>
+                    <!-- Enlace a Bootstrap desde su repositorio remoto -->
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+                  </head>
+                  <body>
+                    <div class="container mt-5" style="margin-bottom: 200px">
+                        <h2>Crear producte</h2>
+                        <hr>
+                        <form action="" method="POST">
+                            <div class="mb-3">
+                                <label for="nom" class="form-label">Nombre:</label>
+                                <input type="text" name="nom" class="form-control" required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="descripcio" class="form-label">Descripción:</label>
+                                <input type="text" name="descripcio" class="form-control" required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="preu" class="form-label">Precio:</label>
+                                <input type="number" name="preu" class="form-control" required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="categoria" class="form-label">Categoría:</label>
+                                <select name="categoria" class="form-select" required>
+                                    <!-- Opciones del selector de categorías con la opción seleccionada según la información actual -->
+                                    <option value="1" >Electrónicos</option>
+                                    <option value="2">Roba</option>
+                                    <!-- Agrega más opciones según sea necesario -->
+                                </select>
+                            </div>
+
+                            <!-- Agrega más campos según sea necesario -->
+
+                            <hr>
+                            <!-- Botones de guardar y cancelar -->
+                            <input type="submit" value="Guardar" class="btn btn-primary">
+                            <a href="Principal.php" class="btn btn-secondary">Cancelar</a>
+                        </form>
+                    </div>';
+            
+            // Incluye el pie de página
+            require_once('Footer.php');
+    }
     public function crear($nom, $descripcio, $preu, $categoria){
-        //la variable id no la demanem ja que és autoincremental!
-        if (!isset($nom) || !isset($descripcio) || !isset($preu) || !isset($categoria)) {
-            echo '<p>Es necessiten tots els camps per crear el producte.</p>';
-            return;
-        }
+        //la variable id no la demanem ja que és autoincremental!                  
         // Crea una instancia de la clase de conexión
         $conexionObj = new Connexio();
         // Obtiene la conexión a la base de datos
         $conexion = $conexionObj->obtenirConnexio();
 
-        
+       
         // Escapa las variables para prevenir SQL injection
         $nom = $conexion->real_escape_string($nom);
         $descripcio = $conexion->real_escape_string($descripcio);
@@ -22,15 +71,12 @@ Class Crear{
         $categoria = $conexion->real_escape_string($categoria);
         
         // Construye la consulta SQL de actualización
-        $consulta = "UPDATE productes
-                     SET nom = '$nom', descripció = '$descripcio', preu = '$preu', categoria_id = '$categoria'
-                     WHERE id = '$id'";
-        
-        "INSERT INTO 'productes'('nom', 'descripció', 'preu', 'categoria_id) "
-        . "VALUES ('$nom','$descripcio','$preu','$categoria')";
+        $consulta = "INSERT INTO productes(nom, descripció, preu, categoria_id) 
+                    VALUES ('$nom','$descripcio','$preu','$categoria')";
 
         // Ejecuta la consulta y redirige a la página principal si tiene éxito
         if ($conexion->query($consulta) === TRUE) {
+            echo $conexion->info;
             header('Location: Principal.php');
             exit();
         } else {
@@ -40,10 +86,10 @@ Class Crear{
 
         // Cierra la conexión a la base de datos
         $conexion->close();
+        }
+        }
+    
 
-    }
-}
-   
 // Obtiene los valores del formulario (si existen)
 
 $nom = isset($_POST['nom']) ? $_POST['nom'] : null;
@@ -51,8 +97,10 @@ $descripcio = isset($_POST['descripcio']) ? $_POST['descripcio'] : null;
 $preu = isset($_POST['preu']) ? $_POST['preu'] : null;
 $categoria = isset($_POST['categoria']) ? $_POST['categoria'] : null;
 
-// Crea una instancia de la clase Actualitzar y llama al método actualizar
-$crearProducto = new Crear();
-$crearProducto->crear($nom, $descripcio, $preu, $categoria);
-
-?>
+// Crea una instancia de la clase Crear y llama al método crear
+$crearProducto = new Nou();
+$crearProducto->mostrarFormulari();
+if (!isset($_POST['nom']) || !isset($_POST['descripcio']) || !isset($_POST['preu']) || !isset($_POST['categoria'])) {
+        echo '<p>Es necessiten tots els camps per crear el producte.</p>';      
+        }else{
+    $crearProducto->crear($nom, $descripcio, $preu, $categoria);}
